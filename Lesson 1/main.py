5e820f9bf02923525b9356987034ca93a502e347
Payal Priyadarshini <payal.priyadarshini@gmail.com>
2024-04-12 15:17:52 +0100

    Python Tutle lessons Year 7

---
+++ Lesson 1/main.py
@@ -0,0 +1,15 @@
+import turtle
+import time
+turtle.Screen().bgcolor("blue")
+
+myTurtle= turtle.Turtle()
+
+myTurtle.goto(10,10)
+#myTurtle.goto(100,100)
+#Extension: change turtle shape, change turtle size
+#change turtle shape: "turtle","arrow","circle","square","arrow","triangle","classic"
+myTurtle.shape("classic")
+myTurtle.shapesize(10,10,10)
+myTurtle.fillcolor("yellow")
+myTurtle.pencolor("green")
+time.sleep(5)
